0be4be44709f4e5bcdaa40c7909c6ebd3dcacd83
Murphywuwu <15908145934@163.com>
2015-12-15 20:26:31 +0800

    my tab code 3

---
+++ package/tab/code/murphywuwu/3/index.js
@@ -0,0 +1,30 @@
+/**
+ * @file tab js file
+ * @author murphywuwu
+ */
+;(function () {
+	tab = function (obj) {
+		// 获取元素
+		var tabNew = $(obj.element);
+		var tabNum = $(obj.triggers);
+		var tabCont = $(obj.contents);
+		// 初始化页面
+		var activeClass = obj.activeTriggerClass;
+		var activeIndex = obj.activeIndex;
+		if (!activeIndex) {
+			tabNum.eq(0).addClass(activeClass);
+			tabCont.eq(0).addClass(activeClass);
+		}else {
+			tabNum.eq(activeIndex).addClass(activeClass);
+			tabCont.eq(activeIndex).addClass(activeClass);
+		}
+		// 实现tab交互
+		tabNew.on('click', obj.triggers, function () {
+			var showTab = $(this);
+			showTab.addClass(activeClass).siblings().removeClass(activeClass);
+
+			var showCont = tabCont.eq(showTab.index());
+			showCont.addClass(activeClass);
+		});
+	};
+})();
