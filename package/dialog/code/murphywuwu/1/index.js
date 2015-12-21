41323519726e949dbf1965be9aa0cac794d8322f
Murphywuwu <15908145934@163.com>
2015-12-21 21:37:46 +0800

    my dialog code 1

--- package/dialog/code/murphywuwu/1/index.js
+++ package/dialog/code/murphywuwu/1/index.js
@@ -2,16 +2,54 @@
  * @file dialog1 js file
  * @author murphywuwu
  */
-!(function  () {
-	// 获取元素
-	var button = $('.dialog-button');
-	var dialog_container = $('.dialog-container');
-	var close = $('.close');
-	// 实现dialog弹出层
-	button.click(function(){
-		dialog_container.show();
-	})
-	close.click(function(){
-		dialog_container.hide();
-	})
-})();
+!(function  (global) {
+	/**
+	 * title : string
+	 * content: string
+	 */
+	function Dialog (opts) {
+		this.init(opts);
+	}
+
+	Dialog.prototype = {
+		init: function (opts) {
+			this.opts =  opts;
+			this._buildHtml(opts);
+			this.bindEvent();
+		},
+		_buildHtml: function (opts) {
+		
+			//  构造HTML结构
+			var html = '<div class="dialog-container">'
+			         + '<div class="dialog">'
+			         + '<div class="title">' 
+			         + opts.title 
+			         + '</div>'
+			         + '<div class="content">'
+			         + opts.content 
+			         + '</div>'
+			         + '<div class="close">'
+			         + 'X'
+			         + '</div>'
+			         + '</div></div>';
+
+			// 将Dialog插入DOM中
+			$('body').append(html);
+
+			this.dialog_container = $('.dialog-container')
+		},
+		bindEvent: function () {
+			this.dialog_container = $('.dialog-container');
+			var that = this;
+			var close = $('.close');
+			close.click(function () {
+				that.dialog_container.hide();
+			})
+		}
+
+	}
+
+	global.Dialog = Dialog;
+
+})(window);
+ 
