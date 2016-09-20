dc088f532163e6ce6bb123d04a89710df2e38861
liuyibin <yibinliu@qq.com>
2016-09-20 14:09:41 +0800

    第三阶段完成

--- package/dialog/code/LastGun/dialog-stage-3/dialog.js
+++ package/dialog/code/LastGun/dialog-stage-3/dialog.js
@@ -1,23 +1,46 @@
-;(function (global) {
-    function $(element){
-        if(element){
-            return document.querySelectorAll(element)[0];
-        }
-    }
+;(function ($,global) {
     function Dialog(option){
         this._init(option);
     }
     Dialog.prototype={
         _init: function (option) {
-
+            this._evenHandler(option);
         },
         _render: function (option) {
-
+            var _html= '<div class="dialog-mask"></div>' +
+                '<div class="dialog-wrapper">' +
+                '<div class="dialog-title">'
+                +option.title
+                +'<button class="dialog-close">X</button>' +
+                '</div>' +
+                '<div class="dialog-content">'
+                +option.content
+                +'</div>' +
+                '</div>';
+            var render=document.querySelector('#container');
+            render.innerHTML=_html;
+            $('.dialog-close')[0].addEventListener('click', function () {
+                if(option||option.effectHide){
+                    option.effectHide($('.dialog-mask,.dialog-wrapper'));
+                }else{
+                    $('.dialog-wrapper').style.display='none';
+                    $('.dialog-mask').style.display='none';
+                }
+            })
         },
         _evenHandler: function (option) {
+            var that=this;
+            $(option.trigger)[0].addEventListener('click', function () {
+                that._render(option);
+                if(option||option.effectShow){
+                    option.effectShow($('.dialog-mask,.dialog-wrapper'));
+                }else{
+                    $('.dialog-wrapper').style.display='block';
+                    $('.dialog-mask').style.display='block';
+                }
 
+            });
         }
-
     }
     global.Dialog=Dialog;
-})(window)
+})(jQuery,window)
